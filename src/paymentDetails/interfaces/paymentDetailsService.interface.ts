9f000948eccc6816b11c0cb5761d56b7e7a5fb8e
Eduardo Fialho <e.fialhosantos@yahoo.com.br>
2023-03-28 22:03:28 -0300

    Added create and findOne to paymentDetails

--- src/paymentDetails/interfaces/paymentDetailsService.interface.ts
+++ src/paymentDetails/interfaces/paymentDetailsService.interface.ts
@@ -1,6 +1,10 @@
 import { paymentStatus } from '@src/common/enums/paymentStatus.enum';
-import { PaymentDetails } from '../entity/paymentDetails.entity';
-//import{}
+import { PaymentDetails } from '@paymentDetails/entity/paymentDetails.entity';
+import { FindOptionsSelect } from 'typeorm';
+import { PaymentDetailsDto } from '@paymentDetails/dtos/paymentDetails.dto';
 //import{}
 
-export interface PaymentDetailsservice {}
+export interface PaymentDetailsService {
+	create(paymentDetails: PaymentDetailsDto): Promise<PaymentDetails>;
+	findOne(id: number): Promise<PaymentDetails>;
+}
---
+++ src/paymentDetails/paymentDetails.service.ts
@@ -0,0 +1,38 @@
+import { Repository } from 'typeorm';
+import { PaymentDetailsService as Service } from '@paymentDetails/interfaces/paymentDetailsService.interface';
+import { PaymentDetailsDto } from '@paymentDetails/dtos/paymentDetails.dto';
+import { HttpException, HttpStatusCode } from '@bse-b2c/common';
+import { PaymentDetails } from './entity/paymentDetails.entity';
+
+export class PaymentDetailsService implements Service {
+	constructor(private repository: Repository<PaymentDetails>) {}
+
+    create = async ({
+        status,
+        provider,
+        type,
+    }:PaymentDetailsDto): Promise<PaymentDetails> => {
+        
+        const newPayment = this.repository.create({
+            status,
+            provider,
+            type,
+            date: new Date(),
+        });
+        return this.repository.save(newPayment);
+    }
+
+    findOne = async(id: number): Promise<PaymentDetails> =>{
+        const payment = await this.repository.findOne({
+            relations:{orderDetails: true},
+            where: {id}
+        })
+
+        if(!payment)
+        throw new HttpException({
+            statusCode: HttpStatusCode.NOT_FOUND,
+            message: `Payment ${id} not found`,
+    });
+
+    return payment;
+}
