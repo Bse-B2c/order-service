40196a596a51476d72a3f02b9b271a5b3b53cd56
Eduardo Fialho <e.fialhosantos@yahoo.com.br>
2023-04-01 02:30:49 -0300

    Added findOne to controller

--- src/paymentDetails/paymentDetails.controller.ts
+++ src/paymentDetails/paymentDetails.controller.ts
@@ -27,4 +27,20 @@ export class PaymentDetailsController {
 			next(e);
 		}
 	};
+
+	findOne = async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const { id } = req.params;
+
+			const response = await this.service.findOne(+id);
+
+			return res.status(HttpStatusCode.OK).send({
+				statusCode: HttpStatusCode.OK,
+				error: null,
+				data: response,
+			});
+		} catch (e) {
+			next(e);
+		}
+	};
 }
