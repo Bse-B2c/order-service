6d7eedeedd80cd46c4d96e7eeee5b1644babfb7f
Eduardo Fialho <e.fialhosantos@yahoo.com.br>
2023-04-10 15:37:49 -0300

    Added crud to service interface

--- src/orderItems/interfaces/orderItemsService.interface.ts
+++ src/orderItems/interfaces/orderItemsService.interface.ts
@@ -1,1 +1,11 @@
-export interface OrderItemsService {}
+import { OrderItemsDto } from '@orderItems/dtos/orderItems.dto';
+import { SearchDto } from '@orderItems/dtos/search.dto';
+import { OrderItems } from '@orderItems/entity/orderItems.entity';
+
+export interface OrderItemsService {
+	create(orderItems: OrderItemsDto): Promise<OrderItems>;
+	findOne(id: number): Promise<OrderItems>;
+	delete(id: number): Promise<OrderItems>;
+	find(search: SearchDto): Promise<Array<OrderItems>>;
+	update(id: number, updatedItems: OrderItemsDto): Promise<OrderItems>;
+}
