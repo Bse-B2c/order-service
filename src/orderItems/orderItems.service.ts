4f37cc8b1dcc7eefe5643ce313b8401c131910a8
Eduardo Fialho <e.fialhosantos@yahoo.com.br>
2023-04-10 12:23:08 -0300

    Added create to order items service

--- src/orderItems/orderItems.service.ts
+++ src/orderItems/orderItems.service.ts
@@ -1,3 +1,23 @@
 import { OrderItemsService as Service } from '@orderItems/interfaces/orderItemsService.interface';
+import { Repository } from 'typeorm';
+import { OrderItems } from '@orderItems/entity/orderItems.entity';
+import { OrderItemsDto } from '@orderItems/dtos/orderItems.dto';
 
-export class OrderItemsService implements Service {}
+export class OrderItemsService implements Service {
+	constructor(private repository: Repository<OrderItems>) {}
+
+	create = async ({
+		quantity,
+		purchaseDate,
+		productId,
+		total,
+	}: OrderItemsDto): Promise<OrderItems> => {
+		const newItems = await this.repository.create({
+			quantity,
+			purchaseDate,
+			productId,
+			total,
+		});
+		return this.repository.save(newItems);
+	};
+}
