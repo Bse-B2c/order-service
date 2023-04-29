1422706641e1dcbcf3890ee1049ac823ba8a1023
Eduardo Fialho <e.fialhosantos@yahoo.com.br>
2023-04-29 20:11:57 -0300

    Created cartItem entity

---
+++ src/cartItem/entity/cartItem.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class CartItem {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column()
+	quantity: number;
+
+	@Column({ default: new Date() })
+	date: Date;
+
+	@Column()
+	productId: number;
+}
