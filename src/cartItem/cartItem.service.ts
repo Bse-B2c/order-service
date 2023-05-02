f06008985fdb642709b373b2f6721e425d684dde
Eduardo Fialho <e.fialhosantos@yahoo.com.br>
2023-05-02 03:47:31 -0300

    Added delete and findone to service

--- src/cartItem/cartItem.service.ts
+++ src/cartItem/cartItem.service.ts
@@ -2,6 +2,7 @@ import { CartItemService as Service } from '@cartItem/interfaces/cartItemService
 import { CartItem } from '@cartItem/entity/cartItem.entity';
 import { Repository } from 'typeorm';
 import { CartItemDto } from '@cartItem/dtos/cartItem.dto';
+import { HttpException, HttpStatusCode } from '@bse-b2c/common';
 
 export class CartItemService implements Service {
 	constructor(private repository: Repository<CartItem>) {}
@@ -18,5 +19,27 @@ export class CartItemService implements Service {
 		});
 
 		return this.repository.save(newCart);
+	};
+
+	findOne = async (id: number): Promise<CartItem> => {
+		const item = await this.repository.findOne({
+			where: { id },
+		});
+
+		if (!item)
+			throw new HttpException({
+				statusCode: HttpStatusCode.NOT_FOUND,
+				message: `Item ${id} not found`,
+			});
+
+		return item;
+	};
+
+	delete = async (id: number) => {
+		const item = await this.findOne(id);
+
+		await this.repository.delete(id);
+
+		return item;
 	};
 }
