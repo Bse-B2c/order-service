234c187ca043f721f732f9dd12e4e3630d4fcaf0
Eduardo Fialho <e.fialhosantos@yahoo.com.br>
2023-05-03 02:02:28 -0300

    Added rest of CRUD to service

--- src/cartItem/cartItem.service.ts
+++ src/cartItem/cartItem.service.ts
@@ -1,8 +1,16 @@
 import { CartItemService as Service } from '@cartItem/interfaces/cartItemService.interface';
 import { CartItem } from '@cartItem/entity/cartItem.entity';
-import { Repository } from 'typeorm';
+import {
+	ArrayContains,
+	FindOptionsOrderValue,
+	FindOptionsWhere,
+	In,
+	MoreThanOrEqual,
+	Repository,
+} from 'typeorm';
 import { CartItemDto } from '@cartItem/dtos/cartItem.dto';
 import { HttpException, HttpStatusCode } from '@bse-b2c/common';
+import { SearchDto } from '@cartItem/dtos/search.dto';
 
 export class CartItemService implements Service {
 	constructor(private repository: Repository<CartItem>) {}
@@ -41,5 +49,49 @@ export class CartItemService implements Service {
 		await this.repository.delete(id);
 
 		return item;
+	};
+
+	find = async (search: SearchDto): Promise<Array<CartItem>> => {
+		const {
+			ids,
+			quantity,
+			date,
+			productId,
+			limit = 10,
+			page = 0,
+
+			sortOrder = 'asc',
+		} = search;
+
+		let where: FindOptionsWhere<CartItem> = {};
+
+		if (ids) where = { ...where, id: In(ids) };
+
+		if (quantity) where = { ...where, quantity: ArrayContains(quantity) };
+
+		if (productId) where = { ...where, productId: ArrayContains(productId) };
+
+		if (date)
+			where = {
+				...where,
+				date: MoreThanOrEqual(new Date(date)),
+			};
+
+		return this.repository.find({
+			where,
+			order: {
+				productId: sortOrder as FindOptionsOrderValue,
+			},
+			take: limit,
+			skip: page * limit,
+		});
+	};
+
+	update = async (id: number, updatedItems: CartItemDto): Promise<CartItem> => {
+		const items = await this.findOne(id);
+
+		Object.assign(items, updatedItems);
+
+		return this.repository.save(items);
 	};
 }
--- src/cartItem/interfaces/cartItemService.interface.ts
+++ src/cartItem/interfaces/cartItemService.interface.ts
@@ -1,1 +1,11 @@
-export interface CartItemService {}
+import { CartItemDto } from '@cartItem/dtos/cartItem.dto';
+import { SearchDto } from '@cartItem/dtos/search.dto';
+import { CartItem } from '@cartItem/entity/cartItem.entity';
+
+export interface CartItemService {
+	create(cartItem: CartItemDto): Promise<CartItem>;
+	findOne(id: number): Promise<CartItem>;
+	delete(id: number): Promise<CartItem>;
+	find(search: SearchDto): Promise<Array<CartItem>>;
+	update(id: number, updatedItems: CartItemDto): Promise<CartItem>;
+}
