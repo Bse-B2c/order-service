30b6aa4d1847d350fe2216c09d4db5efd88a2270
Eduardo Fialho <e.fialhosantos@yahoo.com.br>
2023-05-04 03:22:48 -0300

    Added shoppingCart routes

--- src/routes/api/cart/shopping/index.ts
+++ src/routes/api/cart/shopping/index.ts
@@ -1,5 +1,29 @@
 import { Router } from 'express';
+import { validate } from '@src/common/utils/validate.utils';
 
 const router = Router();
 
+//controller
+import { shoppingCartController } from '@src/shoppingCart';
+
+//dto
+import { ShoppingCartDto } from '@shoppingCart/dtos/shoppingCart.dto';
+import { ParamsDto } from '@src/common/dtos/params.dto';
+import { SearchDto } from '@shoppingCart/dtos/search.dto';
+
+//validate
+const validateBody = validate('body');
+const validateParams = validate('params');
+const validateQuery = validate('query');
+
+router.post('/', validateBody(ShoppingCartDto), shoppingCartController.create);
+router.get('/:id', validateParams(ParamsDto), shoppingCartController.findOne);
+router.get('/', validateQuery(SearchDto), shoppingCartController.find);
+router.delete('/:id', validateParams(SearchDto), shoppingCartController.delete);
+router.patch(
+	'/:id',
+	validateParams(ParamsDto),
+	validateBody(ShoppingCartDto),
+	shoppingCartController.update
+);
 export default router;
