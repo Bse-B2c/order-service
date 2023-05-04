e41f781f55477f9940dccf27e9ef69ad24806656
Eduardo Fialho <e.fialhosantos@yahoo.com.br>
2023-05-04 03:16:16 -0300

    Added shoppingCart interface

---
+++ src/shoppingCart/interfaces/shoppingCartService.interface.ts
@@ -0,0 +1,11 @@
+import { ShoppingCartDto } from '@shoppingCart/dtos/shoppingCart.dto';
+import { SearchDto } from '@shoppingCart/dtos/search.dto';
+import { ShoppingCart } from '@shoppingCart/entity/shoppingCart.entity';
+
+export interface ShoppingCartService {
+	create(shoppingCart: ShoppingCartDto): Promise<ShoppingCart>;
+	findOne(id: number): Promise<ShoppingCart>;
+	delete(id: number): Promise<ShoppingCart>;
+	find(search: SearchDto): Promise<Array<ShoppingCart>>;
+	update(id: number, updatedCart: ShoppingCartDto): Promise<ShoppingCart>;
+}
